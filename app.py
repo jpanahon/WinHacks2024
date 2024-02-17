945d8d7a4fdd3ff67a32b2a014d25d293582980c
ahmadmunim <ahmadmunim0803@gmail.com>
2024-02-17 11:17:05 -0500

    completed backend w/ req. pages

--- app.py
+++ app.py
@@ -3,9 +3,20 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/index")
 def index():
 	return render_template("index.html")
+
+@app.route("/shop/")
+def shop():
+    return render_template("shop.html")
+
+@app.route("/pay/")
+def pay():
+    return render_template("pay.html")
+
+@app.route("/budget/")
+def budget():
+    return render_template("budget.html")
 
 if __name__ == '__main__':
 	app.run(debug=True)
